4affd3f13b8d30655efc30ea7ba4e45a7c5727fc
monabberhossain <mn2world@gmail.com>
2022-11-25 23:32:12 +0600

    advertised section design completed

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -5,14 +5,14 @@ import banner from "../../../assets/banner/banner.png";
 const Banner = () => {
     return (
         <div className="">
-            <div className="py-24">
+            <div className="py-16 lg:py-24">
                 <div className="">
-                    <div className="flex flex-col-reverse md:flex-col-reverse lg:flex-row justify-center px-8 gap-8">
-                        <div>
-                            <h1 className="text-stone-700 text-3xl md:text-4xl lg:text-6xl font-bold leading-tight">
+                    <div className="flex flex-col-reverse md:flex-col-reverse lg:flex-row justify-center px-8 lg:px-0 gap-8">
+                        <div className="mt-6 lg:mt-0">
+                            <h1 className="text-stone-700 text-3xl md:text-4xl lg:text-6xl font-bold lg:font-extrabold leading-tight">
                                 Start buying/selling
                                 <br />
-                                with Trendy Resale
+                                with trendyResale
                             </h1>
                             <p className="text-gray-900 text-xl font-semibold my-7">
                                 You can buy your need and can also list your new
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AdvertisedItems from "../AdvertisedItems/AdvertisedItems";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div className="w-screen lg:px-[12%]">
             <Banner></Banner>
+            <AdvertisedItems></AdvertisedItems>
         </div>
     );
 };
